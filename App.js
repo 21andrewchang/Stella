7e5e82652cfa373e15ab8511a03635a0ffb4171a
Andrew Chang <21andrewch@gmail.com>
2024-03-30 09:30:25 -0500

    feat: tailwind test

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, Text, TouchableOpacity } from "react-native";
+import {
+  ImageBackground,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 justify-center items-center bg-white">
-      <TouchableOpacity className="p-4 bg-black rounded-lg">
-        <Text className="font-bold text-white">Quickplay</Text>
-      </TouchableOpacity>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <ImageBackground>
+      <SafeAreaView className="flex-1 justify-center items-center bg-white">
+        <TouchableOpacity className="p-4 bg-black rounded-lg">
+          <Text className="font-bold text-white">Quickplay</Text>
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
