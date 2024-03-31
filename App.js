83cee64fd644a782988d9d31e030ecca0fa9b9ec
Andrew Chang <21andrewch@gmail.com>
2024-03-31 08:38:27 -0500

    feat: skeleton for lesson complete

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  ImageBackground,
-  SafeAreaView,
-  Text,
-  TouchableOpacity,
-} from "react-native";
+import React from "react";
+import { SafeAreaView, View, Text, TouchableOpacity } from "react-native";
+
+let option1 = "test";
+let option2 = "test";
+let option3 = "test";
+let option4 = "hello????";
 
 export default function App() {
   return (
-    <ImageBackground>
-      <SafeAreaView className="flex-1 justify-center items-center bg-white">
-        <TouchableOpacity className="p-4 bg-black rounded-lg">
-          <Text className="font-bold text-white">Quickplay</Text>
-        </TouchableOpacity>
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    </ImageBackground>
+    <SafeAreaView className="flex-1 justify-between items-center bg-white">
+      <View className="flex-1 justify-center p-4 w-full">
+        <View className="overflow-hidden h-full bg-black rounded-lg shadow-md">
+          <Text className="p-4 text-center text-white">Question 1</Text>
+        </View>
+      </View>
+      <View className="p-4 space-y-4 w-full">
+        <View className="flex flex-row justify-between space-x-4 h-20">
+          <TouchableOpacity className="flex justify-center items-center p-4 w-40 bg-black rounded-lg">
+            <Text className="font-bold text-white">{option1}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="flex justify-center items-center p-4 w-40 bg-black rounded-lg">
+            <Text className="font-bold text-white">{option2}</Text>
+          </TouchableOpacity>
+        </View>
+        <View className="flex flex-row justify-between space-x-4 h-20">
+          <TouchableOpacity className="flex justify-center items-center p-4 w-40 bg-black rounded-lg">
+            <Text className="font-bold text-white">{option3}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="flex justify-center items-center p-4 w-40 bg-black rounded-lg">
+            <Text className="font-bold text-white">{option4}</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
